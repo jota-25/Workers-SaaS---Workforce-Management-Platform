2054a77afa1fb6ded42756d01590aab2ce7575d3
Jota <jr2697949@gmail.com>
2026-05-21 01:37:25 -0500

    fix(invites): update routes for invitation acceptance

--- backend/src/routes/invite.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { acceptInvite } from "../controllers/invite.controller.js";
-
-const router = Router();
-
-router.post("/invite/:token",acceptInvite);
-
-export default router;
